64eaf99cb7c35b7287a87628c071b04ddca5a9fb
Chinh <dinhcongchinh1203@gmail.com>
2021-01-27 15:32:08 +0700

    Initial Commit

---
+++ Gradient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gradient
+//
+//  Created by Chinh Dinh on 1/27/21.
+//  Copyright © 2021 Chinh Dinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
